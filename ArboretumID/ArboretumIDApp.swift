91e43abd6278b0d90fc7ee52de1a1235347d2887
Taylan Unal <taylanu@gmail.com>
2021-11-04 16:22:10 -0400

    Initial Commit

---
+++ ArboretumID/ArboretumIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArboretumIDApp.swift
+//  ArboretumID
+//
+//  Created by Taylan Unal on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ArboretumIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArboretumIDUITests/ArboretumIDUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArboretumIDUITestsLaunchTests.swift
+//  ArboretumIDUITests
+//
+//  Created by Taylan Unal on 11/4/21.
+//
+
+import XCTest
+
+class ArboretumIDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
